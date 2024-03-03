6897032e6881b60cf87a813e847faf7c902de37d
Ian Triggs <iantriggs@gmail.com>
2024-03-03 21:18:50 +1100

    Working zsh theme

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -107,28 +107,11 @@
     };
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "thefuck" ];
-      theme = "powerlevel10k";
+      plugins = [ "git" ];
+      # theme = "powerlevel10k";
     };
   };
 
-  # programs.bash = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   # TODO add your custom bashrc here
-  #   bashrcExtra = ''
-  #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin "
-  #   '';
-
-  #   # set some aliases, feel free to add more or remove some
-  #   shellAliases = {
-  #     k = "kubectl";
-  #     urldecode = "
-  #       python3 - c 'import sys, urllib.parse as ul;
-  #     print(ul.unquote_plus(sys.stdin.read()))'";
-  #     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-  #   };
-  # };
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
